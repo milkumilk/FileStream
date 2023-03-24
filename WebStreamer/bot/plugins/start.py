50c832ec1dc8dacedb4ce7192f7751fbcc582a7e
milkumilk <128312296+milkumilk@users.noreply.github.com>
2023-03-24 19:20:57 +0700

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -11,10 +11,10 @@ from WebStreamer.bot import StreamBot
 async def start(_, m: Message):
     if Var.ALLOWED_USERS and not ((str(m.from_user.id) in Var.ALLOWED_USERS) or (m.from_user.username in Var.ALLOWED_USERS)):
         return await m.reply(
-            "You are not in the allowed list of users who can use me. \
-            Check <a href='https://github.com/EverythingSuckz/TG-FileStreamBot#optional-vars'>this link</a> for more info.",
+            "Anda tidak ada dalam daftar pengguna yang diizinkan yang dapat menggunakan saya. \
+            Check <a href='https://t.me/raingersupport#optional-vars'>Link ini</a> untuk info lebih lanjut.",
             disable_web_page_preview=True, quote=True
         )
     await m.reply(
-        f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link.'
+        f'Hi {m.from_user.mention(style="md")}, Kirimi saya file untuk mendapatkan tautan streaming instan.'
     )
